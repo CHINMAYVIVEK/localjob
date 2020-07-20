46a893525e7bd8e349d1a678011dbb593531664f
Chinmay Vivek <chinmay.vivek@gmail.com>
2020-07-20 19:20:01 +0530

    [FIX]: restructured and deployed

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-   return render_template('index.jinja')
-
-if __name__ == '__main__':
-   app.run(host = '0.0.0.0', port = 5000, debug = True)
-   app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+from app.main import app 
+  
+if __name__ == "__main__":
+   app.run() 
+   # app.run(host = '0.0.0.0', port = 5000, debug = True)
+   # app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
